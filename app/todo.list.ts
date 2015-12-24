de4a3d9dae6bd7f81ad7c59d6e998155c4569837
mana <mana.guru@gmail.com>
2015-12-24 16:38:29 -0700

    ng-model and ng-for with Select and Option elements

--- app/todo.list.ts
+++ app/todo.list.ts
@@ -1,5 +1,5 @@
 import {Component} from 'angular2/core';
-import {CORE_DIRECTIVES} from 'angular2/common';
+import {CORE_DIRECTIVES,NgModel} from 'angular2/common';
 import {TodoService} from "./todo.service"
 import {TodoRenderer} from "./todo.renderer"
 import {StartsWith} from './starts.with.pipe'
@@ -7,17 +7,22 @@ import {StartsWith} from './starts.with.pipe'
 @Component({
     selector: 'todo-list',
     pipes: [StartsWith],
-    directives: [CORE_DIRECTIVES,TodoRenderer],
+    directives: [CORE_DIRECTIVES,TodoRenderer, NgModel],
     template: `
     <div>
-        <todo-renderer *ngFor="#todo of todoService.todos | startsWith:'title':'c'" [todo]="todo">
+        <todo-renderer *ngFor="#todo of todoService.todos | startsWith:'title':selectedLetter" [todo]="todo">
         </todo-renderer>
     </div>
+    <select [(ngModel)]="selectedLetter">
+        <option *ngFor="#letter of letters">{{letter}}</option>
+    </select>
         `
 })
 export class TodoList{
+    letters:string[] = ['a', 'c', 'e', 's'];
+    selectedLetter:string = "e";
+    
     constructor ( public todoService:TodoService) {
-        
     }
 }
 
